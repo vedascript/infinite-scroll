477baea46c10eebc0a0fdca433d692bed4cc5297
Vedansh Wani <me.vedanshwani@gmail.com>
2021-08-03 21:00:08 +0530

    api setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`https://randomuser.me/api/?page=${page}&results=${25}`)
+      .then((res) => setUsers(res.data.results));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Infinite Scroll</h2>
+      <div>
+        {users?.map((user, ind) => (
+          <div key={ind}>{user.name.first}</div>
+        ))}
+      </div>
+      {loading && "Loading........"}
     </div>
   );
 }
